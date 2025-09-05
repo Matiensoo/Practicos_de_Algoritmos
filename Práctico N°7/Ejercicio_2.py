4b6e65d72ccc7f5a678478978ebbc5d96a4ef6c8
Matienso <mati.enrique28@gmail.com>
2025-09-05 09:36:07 -0300

    Modificacion a tp7

---
+++ Práctico N°7/Ejercicio_2.py
@@ -0,0 +1,9 @@
+class Triangle :
+    def __init__ (self, l1, l2, l3, parameter1):
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
+        self.parameter1 = l1 + l2 + l3
+    
+triangle_1 = Triangle (2, 4, 6,)
+ 
